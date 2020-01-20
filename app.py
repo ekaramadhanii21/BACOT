462e03c61680222d07b006863fc975b7bdd24b8d
ekaramadhanii21 <ekaramadhani537@gmail.com>
2020-01-20 12:36:45 +0700

    push kedua

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 
 #test pancing ke heroku supaya keluar dyno
 #test kedua pancing worker supaya muncul...
+#test dynoooooooasd
 
 tw = Twitter()
 
@@ -17,7 +18,7 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "prikitiw" in message: #TRIGGER
+                    if "Sumbar!" in message: #TRIGGER
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
